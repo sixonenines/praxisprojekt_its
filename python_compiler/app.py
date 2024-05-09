7ee889b8ebd526fe4491cebdc6686800229f47a1
Niklas1206 <45467452+Niklas1206@users.noreply.github.com>
2024-05-09 09:53:04 +0200

    Add files via upload
    The compiler

---
+++ python_compiler/app.py
@@ -0,0 +1,31 @@
+import sys
+import io
+from flask import Flask, request
+import contextlib
+
+app = Flask(__name__)
+
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' % name
+
+@app.route('/compile',methods = ['POST', 'GET'])
+def compile_code():
+    result = ""
+    old_stdout = sys.stdout
+    new_stdout = io.StringIO()
+    sys.stdout = new_stdout
+    if request.method == "POST":
+        code = request.form.get('code')
+        try:
+            exec(code)
+            result = sys.stdout.getvalue().strip()
+            sys.stdout = old_stdout
+        except Exception as e:
+            print("Fehler beim Ausführen:", e)
+            result = sys.stdout.getvalue().strip()
+            sys.stdout = old_stdout
+    return ("The Result is: " + str(result))
+
+if __name__ == '__main__':
+    app.run(debug = True)
