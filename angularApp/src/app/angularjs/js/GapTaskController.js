632c9dcd4d9600e5487fd7be702d494749583968
mona2708 <mona.muenstermann@web.de>
2024-05-21 19:14:52 +0200

    Gap-Aufgabenüberprüfung
    Die beiden Gap-Aufgaben können jetzt auch auf Korrektheit überprüft werden.

--- angularApp/src/app/angularjs/js/GapTaskController.js
+++ angularApp/src/app/angularjs/js/GapTaskController.js
@@ -8,29 +8,31 @@ app.controller("GapTaskController", function($scope, CorrectAnswerService) {
     $scope.isAnswered1 = false;
     $scope.isAnswered2 = false;
 
-    $scope.checkGapAnswer1 = function(userAnswer1) {
-        var isCorrect = CorrectAnswerService.checkAnswer('F1C1_1', userAnswer1);
+    $scope.checkGapAnswer1 = function(userAnswer1, taskID) {
+        var isCorrect = CorrectAnswerService.checkAnswer(taskID, userAnswer1);
         $scope.isCorrectAnswer1 = isCorrect;
         $scope.isAnswered1 = true;
         updateCompletionStatus();
     };
 
-    $scope.checkGapAnswer2 = function(userAnswer2) {
-        var isCorrect = CorrectAnswerService.checkAnswer('F1C1_2', userAnswer2);
+    $scope.checkGapAnswer2 = function(userAnswer2, taskID) {
+        var isCorrect = CorrectAnswerService.checkAnswer(taskID, userAnswer2);
         $scope.isCorrectAnswer2 = isCorrect;
         $scope.isAnswered2 = true;
         updateCompletionStatus();
     };
 
     $scope.checkGapAnswers = function(userAnswer1, userAnswer2) {
-        $scope.checkGapAnswer1(userAnswer1);
-        $scope.checkGapAnswer2(userAnswer2);
+        $scope.checkGapAnswer1(userAnswer1, "F1C1_1");
+        $scope.checkGapAnswer2(userAnswer2, "F1C1_2");
+    };
+
+    $scope.checkGapAnswerOneAnswer = function(userAnswer1) {
+        $scope.checkGapAnswer1(userAnswer1, "L1C1");
     };
 
     function updateCompletionStatus() {
-        var isTask1Completed = $scope.isCorrectAnswer1 && !$scope.isAnswered2;
-        var isTask2Completed = $scope.isCorrectAnswer2 && !$scope.isAnswered1;
-        $scope.$parent.tasks[$scope.$parent.currentTaskIndex].isCompleted = isTask1Completed || isTask2Completed;
+        var isTask1Completed = $scope.isCorrectAnswer1 || $scope.isCorrectAnswer2;
+        $scope.$parent.tasks[$scope.$parent.currentTaskIndex].isCompleted = isTask1Completed;
     }
-
 });
