dbd3c2e24f9eddc26a579b50986cbb07b6b70e09
sixonenines <49105737+sixonenines@users.noreply.github.com>
2024-07-02 18:21:59 +0200

    Added solutions

--- angularApp/src/app/angularjs/js/FeedbackService.js
+++ angularApp/src/app/angularjs/js/FeedbackService.js
@@ -4,33 +4,49 @@ app.factory('FeedbackService', function($timeout) {
         V1C1: [
             { text: "Hint 1 for V1C1: The value of 'a' is set twice.", highlight: null },
             { text: "Hint 2 for V1C1: The last assigned value of 'a' is crucial.", highlight: 2 },
-            { text: "Hint 3 for V1C1: The value that is set last will be outputted.", highlight: null }
+            { text: "Hint 3 for V1C1: The value that is set last will be outputted.", highlight: null },
+            { text: "The solution is 20", highlight: null },
         ],
         V3C1: [
             { text: "Hint 1 for V3C1: Consider the sequence of operations.", highlight: null },
             { text: "Hint 2 for V3C1: Check which variables are being used.", highlight: 2 },
-            { text: "Hint 3 for V3C1: Take possible overrides into account.", highlight: null }
+            { text: "Hint 3 for V3C1: Take possible overrides into account.", highlight: null },
+            { text: "The solution is 101", highlight: null },
         ],
         V5C1: [
             { text: "Hint 1 for V5C1: Pay attention to loops and conditions.", highlight: null },
             { text: "Hint 2 for V5C1: Verify the loop termination conditions.", highlight: 2 },
-            { text: "Hint 3 for V5C1: Notice how variables are altered within the loops.", highlight: null }
+            { text: "Hint 3 for V5C1: Notice how variables are altered within the loops.", highlight: null },
+            { text: "The solution is 20 20", highlight: null },
         ], 
         F1C1: [
             { text: "Hint 1 for F1C1: Remember to call the function 'my_func' in line 4.", highlight: null },
             { text: "Hint 2 for F1C1: Create a function named 'my_func' in line 1.", highlight: null },
-            { text: "Hint 3 for F1C1: Check if the arguments in the function 'my_func' are correct.", highlight: null }
+            { text: "Hint 3 for F1C1: Check if the arguments in the function 'my_func' are correct.", highlight: null },
+            { text: "The solution for the first gap is def my_func(): The solution for the second gap is my_func()", highlight: null },
         ],
         L1C1: [
             { text: "Hint 1 for L1C1: Ensure that the loop condition in line 2 is correct.", highlight: null },
             { text: "Hint 2 for L1C1: Don't forget to increment the counter correctly in line 4.", highlight: null},
-            { text: "Hint 3 for L1C1:  Initialize the counter 'i' in line 1 with the correct value.", highlight: null }
+            { text: "Hint 3 for L1C1:  Initialize the counter 'i' in line 1 with the correct value.", highlight: null },
+            { text: `
+                The solution for the first gap is: while
+                `, highlight: null },
         ],
         F1C3: [
             { text: 'Start by defining a function named calculation that accepts two parameters, num1 and num2. Use the def keyword to define the function.', highlight: null },
             { text: 'Inside the function, perform both the addition and the subtraction. Store the results of these operations in separate variables. You need to calculate num1 + num2 and num1 - num2.', highlight: null},
             { text: 'Return the results of the addition and subtraction as a tuple from the function. When calling the function, capture the returned values and print them in a single print statement. Tuples are returned in parentheses, and you can print multiple values separated by a comma.', highlight: null },
-            { text: 'When calling the function, capture both returned values by assigning them to two variables, which can then be printed or used in further calculations.', highlight: null}
+            { text: 'When calling the function, capture both returned values by assigning them to two variables, which can then be printed or used in further calculations.', highlight: null},
+            { text: `
+                The solution is:
+                def calculation(a, b):
+                    addition = a + b
+                    subtraction = a - b
+                    return addition, subtraction
+                result_addition, result_subtraction = calculation(num1, num2)
+                print(result_addition, result_subtraction)
+                `, highlight: null },
         ],
         F2C3: [
             { text: 'Start by defining a function that accepts three parameters. Use the def keyword and name your function something descriptive, such as find_largest.', highlight: null },
@@ -42,51 +58,132 @@ app.factory('FeedbackService', function($timeout) {
             { text: 'Think about how to pass both the name and the salary to the show_employee function. You need to add an extra parameter in the function definition.', highlight: null },
             { text: 'In case the function is called without a salary value, it should use a default value. Consider using default parameter values in Python.', highlight: null},
             { text: 'Pay attention to how the arguments are passed in the function calls. Ensure that the order of the arguments in the calls matches the order in the function definition.', highlight: null },
-            { text: 'If a default salary is intended for cases where the salary is not provided, set a default value for the salary parameter in the function definition. This way, the function can be called with just the name and still work correctly.'}
+            { text: 'If a default salary is intended for cases where the salary is not provided, set a default value for the salary parameter in the function definition. This way, the function can be called with just the name and still work correctly.'},
+            { text: `
+                The solution is:
+                def show_employee(name, salary=46000):
+                print("Name:", name, "Salary:", salary)
+                show_employee("Emma", 55000)
+                show_employee("Ben")
+                `, highlight: null },
         ],
         L2C3: [
             { text: 'Use a loop to iterate through all the numbers from start to end, inclusive. The range function will be helpful here.', highlight: null },
             { text: 'Inside the loop, create a helper function to check if a number is prime. A number is prime if it is greater than 1 and has no divisors other than 1 and itself. You can check for divisors by looping from 2 up to the square root of the number (to optimize performance).', highlight: null},
             { text: 'If the number is determined to be prime, print it. Ensure that you print only the prime numbers without any additional text.', highlight: null },
-            { text: 'Optimize the helper function by checking for divisors only up to the square root of the number, as any factor larger than the square root would have a corresponding factor smaller than the square root.', highlight: null}
+            { text: 'Optimize the helper function by checking for divisors only up to the square root of the number, as any factor larger than the square root would have a corresponding factor smaller than the square root.', highlight: null},
+            { text: `
+                The solution is:
+                def maximum(a, b, c):
+                    if (a >= b) and (a >= c):
+                        largest = a
+                    elif (b >= a) and (b >= c):
+                        largest = b
+                    else:
+                        largest = c
+                    return largest
+                res = maximum(num1, num2, num3)
+                print(res)
+                `, highlight: null },
         ],
         V1C3: [
             { text: 'You need a variable to keep track of the running total as you loop through the numbers from 1 to "end". Initialize this variable to 0 before the loop starts.', highlight: null },
             { text: 'Use a for loop to iterate through the range of numbers from 1 to "end", inclusive. The range function will be helpful here, but remember that it usually goes up to but does not include the stop value.', highlight: null},
             { text: 'Inside the loop, add each number to your sum variable. After the loop finishes, print the result. Make sure to print only the sum without any additional text.', highlight: null },
-            { text: 'After the loop has completed, print the sum variable. Make sure to print only the sum without any additional text.', highlight: null}
+            { text: 'After the loop has completed, print the sum variable. Make sure to print only the sum without any additional text.', highlight: null},
+            { text: `
+                The solution is:
+                sum = 0
+                i = 0
+                while i <= end:
+                    sum += 1
+                    i +=1
+                print(sum)
+                `, highlight: null },
         ],
         L5C1: [
             { text: 'You need to set an initial value for the variable num to begin the countdown. In this case, num starts at 10.', highlight: null },
             { text: '"The while loop should continue to execute as long as num is greater than 3. This means the loop will run and decrement num until it reaches 3.', highlight: null},
             { text: 'In each iteration of the while loop, the value of num is decreased by 1. Pay attention to how this affects the value of num each time the loop runs.', highlight: null },
+            { text: `
+                The solution is:
+                9
+                8
+                7
+                6
+                5
+                4
+                3
+                `, highlight: null },
         ],
         
         F1C2: [
         { text: 'Notice that in the current code, the addition operations are combined into a single line, which makes a a tuple rather than separate variables. You need to perform the addition separately for a and b.', highlight: null },
         { text: 'The function currently does not return any values. To get the correct output, you need to explicitly return the new values of a and b from the function', highlight: null},
         { text: 'The function call add5(3, 2) should store the result, and then you need to print this result. Ensure that the function returns a tuple of the updated values and that this returned value is printed.', highlight: null },
+        { text: `
+The solution is: 
+def add5(a,b):
+return a+5, b+5
+result = add5 (3,2)
+print(result)`
+, highlight: null },
         ],
 
         V2C2: [
             { text: 'When you want to modify a global variable inside a function, you need to declare it as global within the function. This tells Python that you are referring to the global variable rather than creating a local one.', highlight: null },
             { text: 'Check that the global variable c is properly initialized before the function modifies it. This way, the function can correctly increment the value of c.', highlight: null},
             { text: 'Make sure the function is called after defining it to execute the code within the function. This ensures that the global variable is incremented and printed as expected.', highlight: null },
+            { text: `
+                The solution is:
+                c = 1
+                def add():
+                    global c
+                    c = c+2
+                    print(c)
+                add()
+                `, highlight: null },
         ],
         L2C2: [
             { text: 'Within the for loop, print each number generated by the range function. Ensure that the indentation of the print statement places it inside the loop body.', highlight: null },
             { text: 'Use a for loop with the range function to generate numbers from 2 to 10 (inclusive). The range function can start from 2 and step by 2 up to 10 to match the sequence.', highlight: null},
             { text: 'After the for loop completes, print Goodbye! outside of the loop. This ensures that Goodbye! is printed only once after all the numbers have been printed.', highlight: null },
+            { text: `
+                The solution is:
+                for x in range (2,12,2):
+                    print(x)
+                print ("Goodbye!")
+                `, highlight: null },
         ],
         L4C2: [
             { text: 'Within the while loop, include code to increment the count variable by 1 on each iteration. This ensures that the count increases with each iteration of the loop.', highlight: null },
             { text: 'The condition (count < 1) in the while loop is incorrect for achieving the desired output. It should be adjusted to ensure that the loop runs as long as count is less than or equal to 5.', highlight: null},
             { text: 'Inside the while loop, use a print statement to display the current value of count with the format the count is : X, where X is the current value of count.', highlight: null },
+            { text: `
+                The solution is:
+                count = 1
+                while (count<=5):
+                    print("The count is",count)
+                    count+=1
+                print("Finished")
+                `, highlight: null },
+
         ],
             L3C1: [
             { text: 'Remember, conditions usually have a diamond shape and represent decisions, while processes are rectangles that show actions.', highlight: null },
             { text: "Remember to follow the logical flow of your code. Each step should naturally lead to the next. If you find a step that doesn't seem to fit, it might be in the wrong place.", highlight: null},
             { text: 'Take a closer look at your connections. Each node should logically connect to the next step in the process. If a connection seems out of place, consider where it should logically flow to maintain the correct sequence.', highlight: null },
+            { text: 'Todo Show Solutions', highlight: null },
+            { text: `
+                The solution is:
+                for num in range(start, end + 1):
+                    if num > 1:
+                        for i in range(2, num):
+                            if (num % i) == 0:
+                                break
+                        else:
+                            print(num)
+                `, highlight: null },
         ]
 
 
